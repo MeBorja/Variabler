2826e7400284226964cfe46489c6fed842d1dc2f
MeBorja <bloumenne@gmail.com>
2022-03-30 08:40:24 +0200

    Variabler

---
+++ Ilja_Semjonovs_Variabler/main.js
@@ -0,0 +1,31 @@
+// oppgave 1
+// Deklarere betyr å lage. Hvis noen skriver deklarere variabel betyr det å lage veriabel.
+
+let Ilja = "Cool Guy";
+let somalia = 1991;
+let code = fakeCode;
+
+// oppgave 2
+// initialisere betyr starte eller launce code
+
+function Haha() {
+    console.log(Ilja)
+}
+
+console.log(Ilja);
+
+//Oppgave 3
+let JoeMama = "sex"; console.log(JoeMama);
+
+//Oppgave 4
+//Forskjellen mellom "let" og "var" er ikke så stor "let" støtter "block statements" mens "var" gjør ikke det. 
+//Dette betyr at "var" er uttdatert og er definert som "deprecated".
+
+//Oppgave 5
+//En konstant eller "const" kan ikke endres i verdi eller definisjon. Du kan bare deklarere en konst en gang. 
+//Mens en variabel kan "variere" i sin verdi etter sin verdi så mange ganger man vil.
+
+//Oppgave 6
+let length = 16;                                  // Number
+let lastName = "John";                            // String
+let x = {firstName:"idiot", lastName:"blyat"};    // Object
